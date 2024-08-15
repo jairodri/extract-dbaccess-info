693df5fe97598ec65abb3d8f1c9baeef82326332
jairodri <rodrig43@gmail.com>
2024-08-15 19:22:21 +0200

    To use environment variables in main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from getdbinfo import get_db_info
+import os
+from dotenv import load_dotenv
 
-access_db = r'C:/VBSIR/bdparam.mdb'
 
+# Cargar variables desde el archivo .env
+load_dotenv()
+
+access_db = os.getenv('ACCESS_DB_PATH')
 if __name__ == '__main__':
     get_db_info(access_db)
