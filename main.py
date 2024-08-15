99f3ff8e8120e675daba02fd11c408f16d189da4
jairodri <rodrig43@gmail.com>
2024-08-15 19:34:18 +0200

    Add comments in english

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import os
 from dotenv import load_dotenv
 
 
-# Cargar variables desde el archivo .env
+# Load variables from the .env file
 load_dotenv()
 
+# Get the database path from the environment variable
 access_db = os.getenv('ACCESS_DB_PATH')
+
 if __name__ == '__main__':
     get_db_info(access_db)
